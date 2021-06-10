054220ddb53d1e33f0cdc6c41d9b6b6a56b79ebf
Sentakuozisan <17f2002d@shinshu-u.ac.jp>
2021-06-10 17:44:51 +0900

    modify path_follower

--- ros/src/path_follower/src/path_follower_arashi.cpp
+++ ros/src/path_follower/src/path_follower_arashi.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (C) 2021 Naoki Akai.
+ * Copyright (C) 2021 Kazuya Arashi.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
